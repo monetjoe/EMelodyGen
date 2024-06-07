1f0636f222bb4c1950727231e400aa463c0d06dd
admin <admin@users.noreply.github.com>
2024-06-07 12:33:03 +0800

    combine 2 repos

---
+++ processor.py
@@ -0,0 +1,325 @@
+import re
+import random
+import subprocess
+from unidecode import unidecode
+from multiprocessing import Pool
+from music21 import converter, stream
+from lib.add_control_codes import split_txt, run_filter, add_tokens
+from lib.abc_transposition import find_all_abc, transpose_an_abc_text
+from utils import *
+
+
+# midi to xml
+def fix_filename_for_mscore(filename: str):
+    output_name = re.sub(r'[\\/:*?"<>|]', "", unidecode(filename))
+    output_name = output_name.replace("[", "  ").replace("]", "  ")
+    return re.sub(r"\s+", " ", output_name).strip()
+
+
+def midi2xml(input_midi_file: str, output_xml_file: str):
+    filename = rm_ext(os.path.basename(input_midi_file))
+    fixed_name = fix_filename_for_mscore(filename)
+    fixed_mid_name = input_midi_file.replace(filename, fixed_name)
+    if input_midi_file != fixed_mid_name:
+        os.rename(input_midi_file, fixed_mid_name)
+
+    with open(LOG_FILE, "a", encoding="utf-8") as error_file:
+        result = subprocess.run(
+            [MSCORE3, "-o", output_xml_file, fixed_mid_name],
+            stderr=error_file,
+            creationflags=subprocess.CREATE_NO_WINDOW,
+        )
+
+    if result.returncode != 0:
+        add_to_log(
+            f"Failed to convert {input_midi_file} to {output_xml_file} : {result.returncode}"
+        )
+
+
+def batch_midi2mxl(mids_to_xmls: dict, output_xml_dir=XML_INPUT):
+    os.makedirs(output_xml_dir, exist_ok=True)
+    for midi in tqdm(mids_to_xmls, desc="Converting mids to xmls..."):
+        midi2xml(midi, mids_to_xmls[midi])
+
+
+def multi_batch_midi2mxl(
+    input_midi_dir=MIDI_OUTPUT, output_xml_dir=XML_INPUT, multi=True
+):
+    if not os.path.exists(input_midi_dir):
+        print(f"Please extract mids into {input_midi_dir} before this!")
+        exit()
+    else:
+        rm_duplicates(input_midi_dir)
+
+    clean_target_dir(XML_INPUT)
+    mids_to_xmls = {}
+    for rel_root, _, filenames in os.walk(input_midi_dir):
+        for filename in tqdm(filenames, desc="Converting mids to xmls..."):
+            if filename.endswith(".mid"):
+                midi_file = os.path.join(rel_root, filename)
+                label = filename.split("_")[0] + "_"
+                xml_file = label + str2md5(rm_ext(filename)) + ".musicxml"
+                mids_to_xmls[midi_file] = os.path.join(output_xml_dir, xml_file)
+
+    if multi:
+        batches, num_cpu = split_dict_by_cpu(mids_to_xmls)
+        pool = Pool(processes=num_cpu)
+        pool.map(batch_midi2mxl, batches)
+
+    else:
+        batch_midi2mxl(mids_to_xmls, output_xml_dir)
+
+
+# xml augumentation
+def slice_xml(input_file: str, output_folder: str, measures_per_part=20):
+    # Load the .musicxml file
+    score = converter.parse(input_file)
+
+    # Initialize variables
+    current_measures = stream.Part()
+    current_measure_count = 0
+    part_index = 1
+    filename_no_ext = rm_ext(os.path.basename(input_file))
+
+    for part in score.parts:
+        for element in part.getElementsByClass(stream.Measure):
+            current_measures.append(element)
+            current_measure_count += 1
+
+            # Check if we've reached the desired number of measures
+            if current_measure_count == measures_per_part:
+                # Export the current set of measures
+                xml_stream = stream.Score([current_measures])
+                export_path = f"{output_folder}/{filename_no_ext}_{part_index}.musicxml"
+                try:
+                    xml_stream.write("musicxml", fp=export_path)
+                except Exception as e:
+                    add_to_log(f"Failed to slice {input_file} to {export_path} : {e}")
+
+                part_index += 1
+                # Reset for the next batch
+                current_measures = stream.Part()
+                current_measure_count = 0
+
+    # Check if there are any remaining measures to be saved
+    if current_measure_count > 0:
+        # Export the remaining measures
+        xml_stream = stream.Score([current_measures])
+        export_path = f"{output_folder}/{filename_no_ext}_{part_index}.musicxml"
+        try:
+            xml_stream.write("musicxml", fp=export_path)
+        except Exception as e:
+            add_to_log(f"Failed to slice {input_file} to {export_path} : {e}")
+
+
+def slice_xmls(xmls_files: list, output_slice_dir=XML_OUTPUT):
+    os.makedirs(output_slice_dir, exist_ok=True)
+    for xml_file in tqdm(xmls_files, desc="Slicing xmls..."):
+        slice_xml(xml_file, output_slice_dir)
+
+
+def multi_slice_xmls(input_xml_dir=XML_INPUT, multi=True):
+    if not os.path.exists(input_xml_dir):
+        print("Please convert mids to xmls before this!")
+        exit()
+
+    clean_target_dir(XML_OUTPUT)
+    xmls_files = []
+    for rel_root, _, filenames in os.walk(input_xml_dir):
+        for filename in filenames:
+            if (
+                filename.endswith(".xml")
+                or filename.endswith(".musicxml")
+                or filename.endswith(".mxl")
+            ):
+                xmls_files.append(os.path.join(rel_root, filename))
+
+    if multi:
+        batches, num_cpu = split_list_by_cpu(xmls_files)
+        pool = Pool(processes=num_cpu)
+        pool.map(slice_xmls, batches)
+
+    else:
+        slice_xmls(xmls_files)
+
+
+# xml to abc
+def xml2abc(input_xml_file: str, output_abc_file: str):
+    with open(LOG_FILE, "a", encoding="utf-8") as error_file:
+        result = subprocess.run(
+            f"python -Xfrozen_modules=off ./lib/xml2abc.py {input_xml_file}",
+            stdout=subprocess.PIPE,
+            stderr=error_file,
+            text=True,
+        )
+
+    if result.returncode == 0:
+        output = str(result.stdout).strip()
+        if output:
+            with open(output_abc_file, "w", encoding="utf-8") as f:
+                f.write(output)
+
+        else:
+            add_to_log(f"Convert {input_xml_file} to an empty abc")
+
+    else:
+        add_to_log(
+            f"Failed to convert {input_xml_file} to {output_abc_file} : {result.returncode}"
+        )
+
+
+def batch_xml2abc(xmls_to_abcs: dict, output_abc_dir=ABC_INPUT):
+    os.makedirs(output_abc_dir, exist_ok=True)
+    for xml in tqdm(xmls_to_abcs, desc="Converting xmls to abcs..."):
+        xml2abc(xml, xmls_to_abcs[xml])
+
+
+def multi_batch_xml2abc(input_xml_dir=XML_OUTPUT, output_abc_dir=ABC_INPUT, multi=True):
+    if not os.path.exists(input_xml_dir):
+        print("Please slice xmls before this!")
+        exit()
+
+    clean_target_dir(output_abc_dir)
+    xmls_to_abcs = {}
+    for rel_root, _, filenames in os.walk(input_xml_dir):
+        for filename in filenames:
+            if (
+                filename.endswith(".xml")
+                or filename.endswith(".musicxml")
+                or filename.endswith(".mxl")
+            ):
+                xml_file = os.path.join(rel_root, filename)
+                xmls_to_abcs[xml_file] = os.path.join(
+                    output_abc_dir, change_ext(filename, ".abc")
+                )
+
+    if multi:
+        batch, num_cpu = split_dict_by_cpu(xmls_to_abcs)
+        pool = Pool(processes=num_cpu)
+        pool.map(batch_xml2abc, batch)
+
+    else:
+        batch_xml2abc(xmls_to_abcs, output_abc_dir)
+
+
+# abc augumentation
+def transpose_abc(abc_path: str, transposed_abc_dir=ABC_OUTPUT):
+    for tone in TONE_CHOICES:
+        with open(abc_path, "r", encoding="utf-8") as f:
+            abc_text_lines = f.readlines()
+
+        try:
+            transposed_abc_text, _, _ = transpose_an_abc_text(abc_text_lines, tone)
+            abc_name = rm_ext(os.path.basename(abc_path))
+            output_path = f"{transposed_abc_dir}/{abc_name}_{tone}.abc"
+            with open(output_path, "w", encoding="utf-8") as file:
+                file.write(transposed_abc_text)
+
+        except Exception as e:
+            add_to_log(f"Failed to transpose {abc_path} to {tone} : {e}")
+
+
+def transpose_abcs(abc_files: list, transposed_abc_dir=ABC_OUTPUT):
+    os.makedirs(transposed_abc_dir, exist_ok=True)
+    for abc in tqdm(abc_files, desc="Transposing abcs..."):
+        transpose_abc(abc)
+
+
+def multi_transpose_abcs(input_abc_dir=ABC_INPUT, multi=True):
+    if not os.path.exists(input_abc_dir):
+        print("Please convert xml slices to abcs before this!")
+        exit()
+
+    clean_target_dir(ABC_OUTPUT)
+    abc_files = []
+    for abc_path in find_all_abc(input_abc_dir):
+        abc_files.append(abc_path)
+
+    if multi:
+        batches, num_cpu = split_list_by_cpu(abc_files)
+        pool = Pool(processes=num_cpu)
+        pool.map(transpose_abcs, batches)
+
+    else:
+        transpose_abcs(abc_files)
+
+
+def split_abc(abc_path: str):
+    with open(abc_path, "r", encoding="cp437") as file:
+        text = file.read()
+
+    text_parts = text.split("\n\n")
+    filename = rm_ext(os.path.basename(abc_path))
+    for i, part in enumerate(text_parts):
+        piece = part.strip()
+        if piece:
+            with open(f"{ABC_INPUT}/{filename}_{i}.abc", "w", encoding="utf-8") as file:
+                file.write(piece)
+
+
+# generate dataset
+def save_dataset(dataset: list, split_on: bool):
+    random.shuffle(dataset)
+    data_count = len(dataset)
+
+    if split_on:
+        p90 = int(data_count * 0.9)
+        write_jsonl(dataset[:p90], "./data/train.jsonl")
+        write_jsonl(dataset[p90:], "./data/test.jsonl")
+
+    else:
+        write_jsonl(dataset, "./data/dataset.jsonl")
+
+    print(f"{data_count} succeeded in total")
+    add_to_log(f"{data_count} succeeded in total")
+
+
+def create_dataset(transposed_abcs_dir=ABC_OUTPUT, split_on=False):
+    if not os.path.exists(transposed_abcs_dir):
+        print("Please transpose abcs before this!")
+        exit()
+    else:
+        rm_duplicates(transposed_abcs_dir)
+
+    dataset = []
+    empty_count, fail_count = 0, 0
+    # Traverse the path
+    for dirpath, _, filelist in os.walk(transposed_abcs_dir):
+        # Traverse the list of files
+        for this_file in tqdm(filelist, desc="Generating dataset..."):
+            if this_file.endswith(".abc"):
+                filename = os.path.join(dirpath, this_file)
+                content = run_filter(filename)
+                if content:
+                    meta_data, merged_body_data = split_txt(content)
+                    control_code, melody = add_tokens(meta_data, merged_body_data)
+                    if melody:
+                        dataset.append(
+                            {
+                                "prompt": control_code,
+                                "data": f"X:1\n{melody}",
+                                "label": os.path.basename(filename)
+                                .split("_")[0]
+                                .strip(),
+                            }
+                        )
+                    else:
+                        fail_count += 1
+                        add_to_log(f"Failed to extract melody from {filename}")
+
+                else:
+                    empty_count += 1
+                    add_to_log(f"Failed to parse content from {filename}")
+
+    results = f"{fail_count} failed in total\n{empty_count} empty V1 in total"
+    add_to_log(results)
+    print(results)
+    save_dataset(dataset, split_on)
+
+
+if __name__ == "__main__":
+    multi_batch_midi2mxl()
+    multi_slice_xmls()
+    multi_batch_xml2abc()
+    multi_transpose_abcs()
+    create_dataset()
---
+++ utils.py
@@ -0,0 +1,198 @@
+import os
+from tqdm import tqdm
+
+# utils
+MSCORE3 = "D:/Program Files/MuseScore 3/bin/MuseScore3.exe"
+MIDI_OUTPUT = "./data/mids"
+XML_INPUT = "./data/xmls"
+XML_OUTPUT = "./data/slices"
+ABC_INPUT = "./data/abcs"
+ABC_OUTPUT = "./data/trans"
+LOG_FILE = "./data/log.txt"
+TONE_CHOICES = [
+    "Cb",
+    "Gb",
+    "Db",
+    "Ab",
+    "Eb",
+    "Bb",
+    "F",
+    "C",
+    "G",
+    "D",
+    "A",
+    "E",
+    "B",
+    "F#",
+    "C#",
+]
+
+CPU_ALL_IN = True
+
+
+def add_to_log(err_msg: str):
+    with open(LOG_FILE, "a", encoding="utf-8") as file:
+        file.write(f"{err_msg}\n")
+
+
+def rm_ext(filename: str):
+    last_dot_index = len(filename) - 1
+    while last_dot_index >= 0:
+        if filename[last_dot_index] == ".":
+            break
+
+        last_dot_index -= 1
+
+    # 如果找到了点，则去掉后缀
+    if last_dot_index >= 0:
+        return filename[:last_dot_index]
+
+    else:
+        # 如果没有找到点，则返回原始文件名
+        return filename
+
+
+def change_ext(filename: str, ext: str):
+    if ext == "":
+        return rm_ext(filename)
+
+    extension = ext if len(ext) > 0 and ext[0] == "." else f".{ext}"
+    return rm_ext(filename) + extension
+
+
+def split_dict_by_cpu(dictionary: dict):
+    num_cpus = os.cpu_count()
+    if not CPU_ALL_IN:
+        num_cpus -= 1
+
+    if num_cpus is None:
+        num_cpus = 1  # 如果无法获取CPU数量，则默认为1
+
+    split_dicts = [{} for _ in range(num_cpus)]
+    index = 0
+
+    for key, value in dictionary.items():
+        split_dicts[index][key] = value
+        index = (index + 1) % num_cpus
+
+    return split_dicts, num_cpus
+
+
+def split_list_by_cpu(lst: list):
+    num_cpus = os.cpu_count()
+    if not CPU_ALL_IN:
+        num_cpus -= 1
+
+    if num_cpus is None:
+        num_cpus = 1  # 如果无法获取CPU数量，则默认为1
+
+    split_lists = [[] for _ in range(num_cpus)]
+    index = 0
+
+    for item in lst:
+        split_lists[index].append(item)
+        index = (index + 1) % num_cpus
+
+    return split_lists, num_cpus
+
+
+def clean_target_dir(target_dir: str):
+    import shutil
+
+    if os.path.exists(target_dir):
+        shutil.rmtree(target_dir)
+
+    os.makedirs(target_dir)
+
+
+def str2md5(original_string: str):
+    import hashlib
+
+    md5_obj = hashlib.md5()
+    # Update the md5 object with the original string encoded as bytes
+    md5_obj.update(original_string.encode("utf-8"))
+    # Retrieve the hexadecimal representation of the MD5 hash
+    return md5_obj.hexdigest()
+
+
+def write_jsonl(data: list, output_file: str):
+    import json
+
+    with open(output_file, "w", encoding="utf-8") as f:
+        for item in tqdm(data, desc=f"Saving to {output_file}..."):
+            json.dump(item, f)
+            f.write("\n")
+
+
+def rm_duplicates(folder_path: str):
+    import hashlib
+
+    file_hashes = {}
+
+    # 遍历文件夹中的所有文件
+    for root, _, files in os.walk(folder_path):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+
+            # 使用哈希算法计算文件内容的哈希值
+            with open(file_path, "rb") as f:
+                file_content = f.read()
+                file_hash = hashlib.md5(file_content).hexdigest()
+
+            # 如果哈希值已经存在于字典中，则删除重复文件
+            if file_hash in file_hashes:
+                print(f"Removing duplicate file: {file_path}")
+                os.remove(file_path)
+            else:
+                # 否则，将哈希值和文件路径添加到字典中
+                file_hashes[file_hash] = file_path
+
+
+def determine_key_mode(score_path: str):
+    from music21 import converter
+
+    # 读取MusicXML文件
+    score = converter.parse(score_path)
+    # 分析乐谱的调性
+    key_signature = score.analyze("key")
+    # 获取调性模式（大调或小调）
+    mode = key_signature.mode
+    # 返回调性模式
+    return str(mode)
+
+
+def batch_rename(rename_list: list):
+    fail_list = []
+    for srcname in tqdm(rename_list, desc=f"Renaming files with mode label..."):
+        ext = "." + srcname.split(".")[-1]
+        dstname = str2md5(srcname) + ext
+        dirpath = os.path.dirname(srcname)
+        try:
+            mode = determine_key_mode(srcname)
+            os.renames(srcname, f"{dirpath}/{mode}_{dstname}")
+        except PermissionError as e:
+            print(f"\nAdd {srcname} to retry list : {e}")
+            fail_list.append(srcname)
+
+        except Exception as e:
+            add_to_log(f"\nFailed to rename {srcname} : {e}")
+
+    if fail_list:
+        batch_rename(fail_list)
+
+
+def multi_batch_rename(dirpath: str, multi=True):
+    rename_list = []
+    for root, _, files in os.walk(dirpath):
+        for file in tqdm(files, desc="Loading files..."):
+            rename_list.append(os.path.join(root, file))
+
+    if multi:
+        from multiprocessing import Pool
+
+        batches, num_cpu = split_list_by_cpu(rename_list)
+        pool = Pool(processes=num_cpu)
+        pool.map(batch_rename, batches)
+
+    else:
+        batch_rename(rename_list)

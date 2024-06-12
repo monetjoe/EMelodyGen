20fe0aadf549e46d7215274a43a426f60e4482d7
admin <admin@users.noreply.github.com>
2024-06-12 09:30:41 +0800

    add 3 templates

--- templates.py
+++ templates.py
@@ -12,5 +12,28 @@ def whole():
     create_dataset("./data/abcs/outputs")
 
 
+def joint_abcs2xmls():
+    rm_duplicates("./data/abcs/joins")
+    multi_batch_rename("./data/abcs/joins", "./data/abcs/renames")
+    multi_split_abcs2xmls("./data/abcs/renames", "./data/xmls/outputs")
+
+
+def joint_abcs2dataset():
+    joint_abcs2xmls()
+    multi_batch_xml2abc("./data/xmls/outputs", "./data/abcs/inputs")
+    multi_transpose_abcs("./data/abcs/inputs", "./data/abcs/outputs")
+    rm_duplicates("./data/abcs/outputs")
+    create_dataset("./data/abcs/outputs")
+
+
+def xmls2dataset():
+    rm_duplicates("./data/xmls/inputs")
+    multi_batch_rename("./data/xmls/inputs", "./data/xmls/outputs")
+    multi_batch_xml2abc("./data/xmls/outputs", "./data/abcs/inputs")
+    multi_transpose_abcs("./data/abcs/inputs", "./data/abcs/outputs")
+    rm_duplicates("./data/abcs/outputs")
+    create_dataset("./data/abcs/outputs")
+
+
 if __name__ == "__main__":
     whole()
